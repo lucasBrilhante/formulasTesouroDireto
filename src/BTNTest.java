329f13968aa64752c02957c1235dcf11d3262655
Lucas Brilhante <lbrilhante82@gmail.com>
2017-05-10 16:57:50 -0300

    Criando primeiro teste
    Signed-off-by: Jonathan Rufino <jonathan.rufinotkd@gmail.com>
    Signed-off-by: Lucas Brilhante <lbrilhante82@gmail.com>

--- src/BTNTest.java
+++ src/BTNTest.java
@@ -1,17 +1,20 @@
 import static org.junit.Assert.*;
 
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
 public class BTNTest {
 
+	BTN btn;
+	
 	@Before
 	public void setUp() throws Exception {
-	}
 
+	}
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testBTNNulo() {
+		Assert.assertNotNull(btn);
 	}
-
 }
