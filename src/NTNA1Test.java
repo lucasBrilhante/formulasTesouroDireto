b2acc2a8f943cdcf79e47e9777ed5af99adfc455
Jonathan Rufino <jonathan.rufinotkd@gmail.com>
2017-05-18 21:38:56 -0300

    Criando teste para cálculo de amortização
    ussa maior que ussb

--- src/NTNA1Test.java
+++ src/NTNA1Test.java
@@ -11,4 +11,11 @@ public class NTNA1Test {
 		double amortizacao = ntna1.getAmortizacao(3.45, 3.45, 5000);
 		Assert.assertEquals(5000.0, amortizacao, ERRO);
 	}
+	
+	@Test
+	public void valorDolarAtualizacaoMaiorDolarBase() {
+		NTNA1 ntna1 = new NTNA1();
+		double amortizacao = ntna1.getAmortizacao(3.5, 3.45, 5000);
+		Assert.assertEquals(5072.463768116, amortizacao, ERRO);
+	}
 }
