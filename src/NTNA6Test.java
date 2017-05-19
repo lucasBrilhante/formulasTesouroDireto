e2c51daa3573755ba31aef1d5f164b5cb8b1dc7a
Jonathan Rufino <jonathan.rufinotkd@gmail.com>
2017-05-19 00:40:46 -0300

    Criando teste para cálculo do valor nominal atualizado

--- src/NTNA6Test.java
+++ src/NTNA6Test.java
@@ -1,12 +1,16 @@
 import static org.junit.Assert.*;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class NTNA6Test {
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
+	private static final double PRECISAO_6 = PrecisaoHelper.PRECISAO_6;
 
+	@Test
+	public void testCalculoValorNominalAtualizado() {
+		NTNA6 ntna6 = new NTNA6();
+		double valorNominalAtualizado = ntna6.calcularValorNominalAtualizado(1.3, 2.1, 6279.15);
+		Assert.assertEquals(3887.092857143, valorNominalAtualizado, PRECISAO_6);
+	}
 }
