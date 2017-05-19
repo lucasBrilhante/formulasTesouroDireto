b40fbf9d309cde4cf5f75c913aebcd84b8bb1451
Jonathan Rufino <jonathan.rufinotkd@gmail.com>
2017-05-19 00:05:36 -0300

    Adicionando teste para invalidar retorno falso

--- src/NTNA3Test.java
+++ src/NTNA3Test.java
@@ -13,4 +13,11 @@ public class NTNA3Test {
 		double valorNominal = ntna3.calcularValorNominal(2.7, 2.9);
 		Assert.assertEquals(931.034482759, valorNominal, PRECISAO_6);
 	}
+
+	@Test
+	public void testCalculoValorNominal2() {
+		NTNA3 ntna3 = new NTNA3();
+		double valorNominal = ntna3.calcularValorNominal(3.2, 1.7);
+		Assert.assertEquals(1882.352941176, valorNominal, PRECISAO_6);
+	}
 }
