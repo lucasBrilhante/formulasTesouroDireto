74c99b79764305fe210989a9591b6738b743fea6
Jonathan Rufino <jonathan.rufinotkd@gmail.com>
2017-05-19 01:05:41 -0300

    Extraíndo método para cálculo do fator de juros

--- src/NTNA6.java
+++ src/NTNA6.java
@@ -7,8 +7,13 @@ public class NTNA6 extends PrecisaoHelper {
 
 	public double calcularJuros(double vna, String dtp, String dtup, double i) {
 		int diferencaDias = DataHelper.calculaDiferencaDias(dtp, dtup);
-		double fatorJuros = (diferencaDias / 360.0) * (i / 100.0);
+		double fatorJuros = calcularFatorJuros(i, diferencaDias);
 		double juros = vna * fatorJuros;
 		return definirPrecisao(juros, PRECISAO_6);
+	}
+
+	private double calcularFatorJuros(double i, int diferencaDias) {
+		double fatorJuros = (diferencaDias / 360.0) * (i / 100.0);
+		return definirPrecisao(fatorJuros, PRECISAO_8);
 	}
 }
