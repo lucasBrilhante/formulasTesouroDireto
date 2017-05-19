97803d01411e8a9125eef8f7bf28f59704f189c3
Jonathan Rufino <jonathan.rufinotkd@gmail.com>
2017-05-19 00:52:44 -0300

    Criando método para cálculo do juros

--- src/NTNA6.java
+++ src/NTNA6.java
@@ -4,4 +4,8 @@ public class NTNA6 {
 	public double calcularValorNominalAtualizado(double ussa, double ussb, double ve) {
 		return (ussa / ussb) * ve;
 	}
+
+	public double calcularJuros(double vna, String dtp, String dtup, int i) {
+		return 0;
+	}
 }
