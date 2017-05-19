e3917b990ff6db035cb267aeb29563723a6974f4
Jonathan Rufino <jonathan.rufinotkd@gmail.com>
2017-05-18 23:11:33 -0300

    Refatorando corpo de métodos
    Métodos afetados:
    calculaJuros
    calculaFator

--- src/NTNA1.java
+++ src/NTNA1.java
@@ -1,15 +1,17 @@
 
 public class NTNA1 {
 
-	public double getAmortizacao(double ussa, double ussb, int vl) {
+	public double calculaAmortizacao(double ussa, double ussb, int vl) {
 		return (ussa / ussb) * vl;
 	}
 
-	public double getJuros(int puam, String dtp, String dtup, int i) {
-		return puam * calculaFator(dtp, dtup, i);
+	public double calculaJuros(int puam, String dtp, String dtup, int i) {
+		double fatorJuros = calculaFator(dtp, dtup, i);
+		return puam * fatorJuros;
 	}
 
 	private double calculaFator(String dtp, String dtup, double i) {
-		return (DataHelper.calculaDiferencaDias(dtup, dtp) / 360.0) * (i / 100.0);
+		int diferencaDias = (DataHelper.calculaDiferencaDias(dtup, dtp));
+		return (diferencaDias / 360.0) * (i / 100.0);
 	}
 }
--- src/NTNA1Test.java
+++ src/NTNA1Test.java
@@ -8,35 +8,35 @@ public class NTNA1Test {
 	@Test
 	public void valorDolarAtualizacaoIgualDolarBase() {
 		NTNA1 ntna1 = new NTNA1();
-		double amortizacao = ntna1.getAmortizacao(3.45, 3.45, 5000);
+		double amortizacao = ntna1.calculaAmortizacao(3.45, 3.45, 5000);
 		Assert.assertEquals(5000.0, amortizacao, PRECISAO_6);
 	}
 	
 	@Test
 	public void valorDolarAtualizacaoMaiorDolarBase() {
 		NTNA1 ntna1 = new NTNA1();
-		double amortizacao = ntna1.getAmortizacao(3.5, 3.45, 5000);
+		double amortizacao = ntna1.calculaAmortizacao(3.5, 3.45, 5000);
 		Assert.assertEquals(5072.463768116, amortizacao, PRECISAO_6);
 	}
 	
 	@Test
 	public void valorDolarAtualizacaoMenorDolarBase() {
 		NTNA1 ntna1 = new NTNA1();
-		double amortizacao = ntna1.getAmortizacao(3.3, 3.45, 5000);
+		double amortizacao = ntna1.calculaAmortizacao(3.3, 3.45, 5000);
 		Assert.assertEquals(4782.608695652, amortizacao, PRECISAO_6);
 	}
 	
 	@Test
 	public void deveRetornarJuros() {
 		NTNA1 ntna1 = new NTNA1();
-		double juros = ntna1.getJuros(3000, "03/05/2017", "04/04/2017", 15);
+		double juros = ntna1.calculaJuros(3000, "03/05/2017", "04/04/2017", 15);
 		Assert.assertEquals(36.25, juros, PRECISAO_6);
 	}
 	
 	@Test
 	public void deveRetornarJuros2() {
 		NTNA1 ntna1 = new NTNA1();
-		double juros = ntna1.getJuros(2000, "02/05/2017", "20/04/2017", 5);
+		double juros = ntna1.calculaJuros(2000, "02/05/2017", "20/04/2017", 5);
 		Assert.assertEquals(3.333333333, juros, PRECISAO_6);
 	}
 }
