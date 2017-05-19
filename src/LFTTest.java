00e44daeb22e4fbacebf33e75bdd7a9df667ce35
Lucas Brilhante <lbrilhante82@gmail.com>
2017-05-19 09:38:46 -0300

    Criando classe de teste de LTN

--- src/LFTTest.java
+++ src/LFTTest.java
@@ -64,7 +64,7 @@ public class LFTTest {
 	@Test
 	public void testeLFTcalcLFT() {
 		
-		double lft = LFT.calcLFT(900.00, 1);
+		double lft = LFT.calcLFT(900.00, 1.000495624);
 		Assert.assertEquals(1.000495624*900, lft, 0.00000001);
 	}
 	@Test
---
+++ src/LTNTest.java
@@ -0,0 +1,19 @@
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import junit.framework.Assert;
+
+public class LTNTest {
+	LTN ltn
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		Assert.assertNotNull(ltn);
+	}
+
+}
