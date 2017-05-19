0deaaa12f7053ce31f8db2d9e8c1c36bd0087b19
Jonathan Rufino <jonathan.rufinotkd@gmail.com>
2017-05-18 22:27:24 -0300

    Implementando método para calcular juros

--- src/NTNA1.java
+++ src/NTNA1.java
@@ -4,4 +4,8 @@ public class NTNA1 {
 	public double getAmortizacao(double ussa, double ussb, int vl) {
 		return (ussa / ussb) * vl;
 	}
+
+	public double getJuros(int puam, String dtp, String dtup, int i) {
+		return 0;
+	}
 }
