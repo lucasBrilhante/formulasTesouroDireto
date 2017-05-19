58c05e226e9b4223d7cb0f82122087fb3488e4a0
Lucas Brilhante <lbrilhante82@gmail.com>
2017-05-18 23:05:13 -0300

    Adicionando testes para triangulação

--- src/LFTTest.java
+++ src/LFTTest.java
@@ -23,4 +23,16 @@ public class LFTTest {
 		double celic = LFT.calcCelic(5);
 		Assert.assertEquals(0.00019363, celic,0.00000001);
 	}
+	
+	@Test
+	public void testeLFTCelic2() {
+		double celic = LFT.calcCelic(3);
+		Assert.assertEquals(0.00011730, celic,0.00000001);
+	}
+	
+	@Test
+	public void testeLFTCelic3() {
+		double celic = LFT.calcCelic(10);
+		Assert.assertEquals(0.00037828, celic,0.00000001);
+	}
 }
