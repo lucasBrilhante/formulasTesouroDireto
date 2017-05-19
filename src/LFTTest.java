51887b751dc8a62ea8fb4bdb863f4eb2c4376070
Lucas Brilhante <lbrilhante82@gmail.com>
2017-05-18 22:42:15 -0300

    Fazendo teste passar

--- src/LFTTest.java
+++ src/LFTTest.java
@@ -1,5 +1,6 @@
 import static org.junit.Assert.*;
 
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -8,12 +9,13 @@ public class LFTTest {
 	LFT lft;
 	@Before
 	public void setUp() throws Exception {
-	
+		lft = new LFT();
 	}
 
 	@Test
 	public void testLFTNull() {
-		fail("Not yet implemented");
+		Assert.assertNotNull(lft);
 	}
+	
 
 }
