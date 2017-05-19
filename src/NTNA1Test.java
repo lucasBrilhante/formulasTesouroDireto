f91a131e67a792f95323c59c489001efec63eea3
Jonathan Rufino <jonathan.rufinotkd@gmail.com>
2017-05-18 21:28:55 -0300

    Criando teste para cálculo de amortização

--- src/NTNA1Test.java
+++ src/NTNA1Test.java
@@ -1,11 +1,14 @@
-import static org.junit.Assert.*;
-
+import org.junit.Assert;
 import org.junit.Test;
 
 public class NTNA1Test {
 
+	private static final double ERRO = 0.000001;
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void valorDolarAtualizacaoIgualDolarBase() {
+		NTNA1 ntna1 = new NTNA1();
+		double amortizacao = ntna1.getAmortizacao(3.45, 3.45, 5000);
+		Assert.assertEquals(5000.0, amortizacao, ERRO);
 	}
 }
