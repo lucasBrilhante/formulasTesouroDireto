19e781415f19799a1b3766ce1147ea755ccd0602
Jonathan Rufino <jonathan.rufinotkd@gmail.com>
2017-05-19 01:04:00 -0300

    Refatorando método para cálculo de juros

--- src/NTNA6.java
+++ src/NTNA6.java
@@ -1,11 +1,14 @@
 
-public class NTNA6 {
+public class NTNA6 extends PrecisaoHelper {
 
 	public double calcularValorNominalAtualizado(double ussa, double ussb, double ve) {
 		return (ussa / ussb) * ve;
 	}
 
 	public double calcularJuros(double vna, String dtp, String dtup, double i) {
-		return 105.833333333;
+		int diferencaDias = DataHelper.calculaDiferencaDias(dtp, dtup);
+		double fatorJuros = (diferencaDias / 360.0) * (i / 100.0);
+		double juros = vna * fatorJuros;
+		return definirPrecisao(juros, PRECISAO_6);
 	}
 }
