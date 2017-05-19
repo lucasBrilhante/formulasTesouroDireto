9aac2756ea2c1a877f9d57daeac4f5edcb95468b
Jonathan Rufino <jonathan.rufinotkd@gmail.com>
2017-05-19 00:30:35 -0300

    Refatorando método para cálculo de juros

--- src/NTNA3.java
+++ src/NTNA3.java
@@ -7,6 +7,13 @@ public class NTNA3 extends PrecisaoHelper {
 	}
 
 	public double calcularJuros(double vna, double i, int n) {
-		return vna * ((i / 100.0) * (n / 12.0));
+		double fatorJuros = calcularFator(i, n);
+		double juros = vna * fatorJuros;
+		return definirPrecisao(juros, PRECISAO_6);
+	}
+
+	private double calcularFator(double i, int n) {
+		double fatorJuros = (i / 100.0) * (n / 12.0);
+		return definirPrecisao(fatorJuros, PRECISAO_8);
 	}
 }
