7fea19505bf96e5a246215bf8cd9f7023dae0eea
Lucas Brilhante <lbrilhante82@gmail.com>
2017-05-19 15:21:27 -0300

    Criando classe de teste e classe de LFTB junto com teste de nulo que não passa

---
+++ src/LFTB.java
@@ -0,0 +1,4 @@
+
+public class LFTB extends LFT{
+
+}
---
+++ src/LFTBTest.java
@@ -0,0 +1,19 @@
+import static org.junit.Assert.*;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class LFTBTest {
+
+	LFTB lftb;
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testNotNull(){
+		Assert.assertNotNull(lftb);
+	}
+
+}
