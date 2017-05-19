ab16da4f8416138a88fffcd3aec00cd91bb94b16
Jonathan Rufino <jonathan.rufinotkd@gmail.com>
2017-05-19 00:12:28 -0300

    Criando teste para cálculo de juros

--- src/NTNA3Test.java
+++ src/NTNA3Test.java
@@ -18,4 +18,11 @@ public class NTNA3Test {
 		double valorNominal = ntna3.calcularValorNominal(3.2, 1.7);
 		Assert.assertEquals(1882.352941176, valorNominal, PRECISAO_6);
 	}
+
+	@Test
+	public void testCalculoJuros() {
+		NTNA3 ntna3 = new NTNA3();
+		double juros = ntna3.calcularJuros(3500, 0.023333333);
+		Assert.assertEquals(81.666666667, juros, PRECISAO_6);
+	}
 }
