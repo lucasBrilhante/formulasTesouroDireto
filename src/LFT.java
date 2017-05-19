a804db4972ff06fc38687b90e46a9a5ddef58c67
Lucas Brilhante <lbrilhante82@gmail.com>
2017-05-19 08:43:59 -0300

    Fazendo testes para calc celic passar

--- src/LFT.java
+++ src/LFT.java
@@ -3,7 +3,10 @@ public class LFT {
 
 	public static double calcCelic(double taxa) {
 		// TODO Auto-generated method stub
-		return 0.00019363;
+		double x = (taxa/100)+1;
+		double w = 0.003968254;
+		double y = Math.pow(x,w);
+		return y-1;
 	}
 
 }
