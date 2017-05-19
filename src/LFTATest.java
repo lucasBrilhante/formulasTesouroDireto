90d4774189b8b86c5df831869d2d30020c33ab2d
Lucas Brilhante <lbrilhante82@gmail.com>
2017-05-19 13:45:19 -0300

    Creating Test class and LFTA class with no passing test null

---
+++ src/LFTATest.java
@@ -0,0 +1,20 @@
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import junit.framework.Assert;
+
+public class LFTATest {
+
+	LFTA lfta;
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void notNull() {
+		Assert.assertNotNull(lfta);
+	}
+
+}
