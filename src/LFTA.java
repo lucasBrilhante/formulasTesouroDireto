daa0942134a8b510ad7006e102d9637f63c95673
Lucas Brilhante <lbrilhante82@gmail.com>
2017-05-19 14:39:00 -0300

    Criando teste para calculo de VNa

--- src/LFTA.java
+++ src/LFTA.java
@@ -1,4 +1,10 @@
 
 public class LFTA extends LFT{
+	//C VE Tacr du dut P N
+
+	public double calcVNa(double c, double ve, double tacr, int du, int dut, int P, int N) {
+		
+		return 0;
+	}
 
 }
--- src/LFTATest.java
+++ src/LFTATest.java
@@ -14,8 +14,15 @@ public class LFTATest {
 	}
 
 	@Test
-	public void notNull() {
+	public void testNotNull() {
 		Assert.assertNotNull(lfta);
 	}
+	
+	@Test
+	public void testCalcVNa(){
+		double C = lfta.calcC(0.0001,0.0002);
+		double result = lfta.calcVNa(C,10000,0.0245,30,30,10,1);
+		Assert.assertEquals(8879.248217, result,0.000001);
+	}	
 
 }
