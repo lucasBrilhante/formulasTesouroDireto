2cc7e0404b0ef987fb021d72fe1a499cc1381dfa
Jonathan Rufino <jonathan.rufinotkd@gmail.com>
2017-05-21 18:22:07 -0300

    Refatorando método para cálculo do valor nominal

--- src/NTNB.java
+++ src/NTNB.java
@@ -1,7 +1,16 @@
 
-public class NTNB {
+public class NTNB extends PrecisaoHelper {
 
-	public double calcularValorNominal(double ipcat, double ipcat0, double vn, int dc, int dct) {
-		return 15396.0071783;
+	public double calcularValorNominal(double ipcat, double ipcat0, double ipcat1, double vn, int dc, int dct) {
+		double fator = calcularFator(ipcat0, ipcat1, dc, dct);
+		double valorNominal = fator * (ipcat / ipcat0) * vn;
+		return definirPrecisao(valorNominal, PRECISAO_6);
+	}
+
+	private double calcularFator(double ipcat0, double ipcat1, int dc, int dct) {
+		double base = ipcat1 / ipcat0;
+		double expoente = (double) dc / dct;
+		double fator = Math.pow(base, expoente);
+		return definirPrecisao(fator, PRECISAO_6);
 	}
 }
