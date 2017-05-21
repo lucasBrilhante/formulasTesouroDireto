453e9f9ee41228a6971b4f7f60470e191acc4cd7
Jonathan Rufino <jonathan.rufinotkd@gmail.com>
2017-05-21 17:50:19 -0300

    Adicionando teste para invalidar retorno falso

--- src/NTNBTest.java
+++ src/NTNBTest.java
@@ -11,4 +11,11 @@ public class NTNBTest {
 		double valorNominal = ntnb.calcularValorNominal(2000, 1500, 10000, 10, 20);
 		Assert.assertEquals(15396.0071783, valorNominal, PRECISAO_6);
 	}
+	
+	@Test
+	public void testCalculoValorNominal2() {
+		NTNB ntnb = new NTNB();
+		double valorNominal = ntnb.calcularValorNominal(1700, 2500, 7000, 5, 3);
+		Assert.assertEquals(2502.9670968, valorNominal, PRECISAO_6);
+	}
 }
