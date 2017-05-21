14f04f1fe4f0c5b339d01c6f4bce1a2f51079018
Jonathan Rufino <jonathan.rufinotkd@gmail.com>
2017-05-21 18:36:48 -0300

    Refatorando método para cálculo do juros

--- src/NTNB.java
+++ src/NTNB.java
@@ -13,4 +13,18 @@ public class NTNB extends PrecisaoHelper {
 		double fator = Math.pow(base, expoente);
 		return definirPrecisao(fator, PRECISAO_6);
 	}
+	
+	public double calcularJuros(double vna, int dcp, int dct, int n, double i) {
+		double fatorJuros = calcularFatorJuros(dcp, dct, n, i);
+		double juros = vna * (fatorJuros - 1);
+		return definirPrecisao(juros, PRECISAO_6);
+	}
+	
+	private double calcularFatorJuros(int dcp, int dct, int n, double i) {
+		double base = (i / 100.0) + 1;
+		base = Math.pow(base, (double) n / 12);
+		double expoente = (double) dcp / dct;
+		double fator = Math.pow(base, expoente);
+		return definirPrecisao(fator, PRECISAO_8);
+	}
 }
