740706c8612b692bba28f1353cd52b674e7a0973
Jonathan Rufino <jonathan.rufinotkd@gmail.com>
2017-05-21 17:32:07 -0300

    Criando teste para cálculo do valor nominal

--- src/NTNBTest.java
+++ src/NTNBTest.java
@@ -3,9 +3,13 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class NTNBTest {
+	
+	private static final double PRECISAO_6 = PrecisaoHelper.PRECISAO_6;
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testCalculoValorNominal() {
+		NTNB ntnb = new NTNB();
+		double valorNominal = ntnb.calcularValorNominal(2000, 1500, 10000, 10, 20);
+		Assert.assertEquals(15396.0071783, valorNominal, PRECISAO_6);
 	}
 }
